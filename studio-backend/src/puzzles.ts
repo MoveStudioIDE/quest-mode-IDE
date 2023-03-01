75e3238030fefba4e1fbea22a03a4fe940dfdf2b
Evan W <ewitulsk@calpoly.edu>
2023-02-28 17:50:41 -0800

    actually committed?

---
+++ studio-backend/src/puzzles.ts
@@ -0,0 +1,53 @@
+import * as fs from 'fs';
+import { transferChallengeToml } from './challenges';
+import { compile, testPackage } from './compile';
+import { makeRandString } from './helpers';
+import { Project } from './types';
+
+const TEMP_DIR = `${__dirname}/../temp-packages`;
+const PUZZLES_DIR = `${__dirname}/../puzzles`;
+
+export async function assemblePuzzle(project: Project, puzzleName: string, outputDir: string): Promise<string> {
+    
+    // Created temporary project in user directory
+    const tempProjectPath = `${outputDir}/${puzzleName+makeRandString(20)}`;
+  
+    // Create the project directory
+    fs.mkdirSync(tempProjectPath, { recursive: true });
+  
+    // Created the project's sources directory
+    const tempProjectSourcesPath = `${tempProjectPath}/sources`;
+    fs.mkdirSync(tempProjectSourcesPath, { recursive: true });
+  
+    // Add the module files to the project's sources directory
+    project.templates.forEach((module) => {
+      fs.writeFileSync(`${tempProjectSourcesPath}/${module.name}.move`, module.code);
+    });
+  
+    fs.readFileSync(`${__dirname}/../puzzles/${puzzleName}/Move.toml`, 'utf-8')
+  
+    fs.writeFileSync(
+      `${tempProjectPath}/Move.toml`,
+      fs.readFileSync(`${__dirname}/../puzzles/${puzzleName}/Move.toml`, 'utf-8')
+    )
+  
+    return tempProjectPath;
+  }
+
+export async function compilePuzzle(project: Project){
+    const challengeName = project.challenge.split('%')[1].toLowerCase();
+    const puzzlePath = PUZZLES_DIR+`/${challengeName}`;
+    const projectPath = await assemblePuzzle(project, challengeName, TEMP_DIR)
+    transferChallengeToml(puzzlePath, projectPath);
+    const compileResult = await compile(challengeName, projectPath);
+    return compileResult;
+}
+
+export async function testPuzzle(project: Project){
+    const challengeName = project.challenge.split('%')[1].toLowerCase();
+    const puzzlePath = PUZZLES_DIR+`/${challengeName}`;
+    const projectPath = await assemblePuzzle(project, challengeName, TEMP_DIR)
+    transferChallengeToml(puzzlePath, projectPath);
+    const testResult = await testPackage(projectPath);
+    return testResult;
+}
---
+++ studio-backend/src/test/puzzles.test.ts
@@ -0,0 +1,42 @@
+import { compile, submit, testPackage } from "../compile";
+import * as fs from 'fs';
+import { testPuzzle } from "../puzzles";
+
+const code1 = fs.readFileSync("./puzzles/hello_blockchain/sources/hello_blockchain.move");
+const code2 = fs.readFileSync("./puzzles/hello_blockchain/sources/hello_blockchain_test.move");
+const project = {
+    challenge: 'puzzle%hello_blockchain',
+    templates: [
+        {
+            name: 'hello_blockchain',
+            code: code1,
+        },
+        {
+            name: 'hello_blockchain_test',
+            code: code2,
+        },
+    ],
+    // dependencies: [
+    //     {
+    //         name: "hello_blockchain",
+    //         address: "0x0"
+    //     },
+    // ],
+};
+
+// test("Tests Aptos compile", async () => {
+//     const res = await compile(project);
+//     console.log(res.compiledModules)
+// })
+
+test("Tests Aptos test", async () => {
+    
+    const testResults = await testPuzzle(project);
+    console.log(testResults.errorCode) //Fails Because 
+})
+
+// test("Tests submit and Callback", async () => {
+//     const res = await submit(project, "greenpeppers100", " https://webhook.site/d82b7724-bccc-4b76-b94a-8974849bf13a")
+
+// })
+
